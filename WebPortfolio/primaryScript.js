5bf0bc30d1c128e272f9b9bb9d322401a57566e8
Ethanc1J <ecornwell2@gmail.com>
2018-10-30 16:24:45 -0500

    Added WebPortfolio folder

---
+++ WebPortfolio/primaryScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#nameHeading").hide().fadeIn(900);
+  $("#developItem").hide().fadeIn(1200);  //Loading heading
+  $("#designItem").hide().fadeIn(1500);
+  $("#programItem").hide().fadeIn(1800);
+  $("#smallSummary").hide().fadeIn(1855);
+
+
+
+});
